43ba496ca99c6e6d0628396176eae120334a3588
Seaer <seaerjr@gmail.com>
2023-06-17 11:14:23 +0300

    Setup of configuration to utilize the builder for accessing the appsettings.json file and referencing the required connection string to the database.

---
+++ MyContactManagerData/MyContactManagerDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace MyContactManagerData
+{
+    public class MyContactManagerDbContext : DbContext 
+    {
+        private static IConfigurationRoot _configuration; //will allow to use a builder to get data config string
+
+        public MyContactManagerDbContext()  //scaffold
+        {
+
+        }
+                                                                     //leverage connection strings from appsettings
+        public MyContactManagerDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {//to bring in connection string
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+                _configuration = builder.Build();
+            var cnstr = _configuration.GetConnectionString("MyContactManager");
+            optionsBuilder.UseSqlServer(cnstr);
+                
+         }
+    }
+}
