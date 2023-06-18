ec6d8acc55e2613692fb4b7d7c5662b0e41046d1
Seaer <seaerjr@gmail.com>
2023-06-18 06:30:08 +0300

    Contacts controlled + states controller added. Fixed issue with repeated state calls on edit

--- ContactWebModels/Contact.cs
+++ ContactWebModels/Contact.cs
@@ -6,88 +6,102 @@ namespace ContactWebModels
     {
         [Key]
         public int Id { get; set; }
+
         [Display(Name = "First Name")]
-        [Required(ErrorMessage = "First Name is required.")]
+        [Required(ErrorMessage = "First Name is required")]
         [StringLength(ContactManagerConstants.MAX_FIRST_NAME_LENGTH)]
         public string FirstName { get; set; }
+
         [Display(Name = "Last Name")]
-        [Required(ErrorMessage = "Lasr Name is required.")]
+        [Required(ErrorMessage = "Last Name is required")]
         [StringLength(ContactManagerConstants.MAX_LAST_NAME_LENGTH)]
         public string LastName { get; set; }
-        [Display(Name = "EMAIL Address")]
-        [Required(ErrorMessage = "Email is required.")]
+
+        [Display(Name = "Email Address")]
+        [Required(ErrorMessage = "Email Address is required")]
         [StringLength(ContactManagerConstants.MAX_EMAIL_LENGTH)]
-        [EmailAddress(ErrorMessage = "wrong format")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
         public string Email { get; set; }
-        [Display(Name = "PHONE number")]
-        [Required(ErrorMessage = "PHONE is required.")]
+
+        [Display(Name = "Mobile Phone")]
+        [Required(ErrorMessage = "Phone number is required")]
         [StringLength(ContactManagerConstants.MAX_PHONE_LENGTH)]
-        [Phone(ErrorMessage = "wrong format")]
+        [Phone(ErrorMessage = "Invalid Phone Number")]
         public string PhonePrimary { get; set; }
-        [Display(Name = "Home/Office number")]
-        [Required(ErrorMessage = "PHONE is required.")]
+
+        [Display(Name = "Home/Office Phone")]
+        [Required(ErrorMessage = "Phone number is required")]
         [StringLength(ContactManagerConstants.MAX_PHONE_LENGTH)]
-        [Phone(ErrorMessage = "wrong format")]
+        [Phone(ErrorMessage = "Invalid Phone Number")]
         public string PhoneSecondary { get; set; }
-        [Display(Name = "St Address Line 1")]
+
+        [DataType(DataType.Date)]
+        public DateTime Birthday { get; set; }
+
+        [Display(Name = "Street Address Line 1")]
         [StringLength(ContactManagerConstants.MAX_STREET_ADDRESS_LENGTH)]
         public string StreetAddress1 { get; set; }
-        [Display(Name = "St Address Line 2")]
+
+        [Display(Name = "Street Address Line 2")]
         [StringLength(ContactManagerConstants.MAX_STREET_ADDRESS_LENGTH)]
-        public virtual State State { get; set; }
         public string StreetAddress2 { get; set; }
-        [Required(ErrorMessage = "City is Required")]
+
+        [Required(ErrorMessage = "City is required")]
         [StringLength(ContactManagerConstants.MAX_CITY_LENGTH)]
         public string City { get; set; }
-        [Required(ErrorMessage = "Zip is required")]
-        [Display(Name = "Zip Code")]
-        [StringLength(ContactManagerConstants.MAX_CITY_LENGTH, MinimumLength = ContactManagerConstants.MIN_ZIP_CODE_LENGTH)]
-        [RegularExpression("(^\\d{5}(-\\d{4})?$)|(^[ABCEGHJKLMNPRSTVXY]{1}\\d{1}[A-Z]{1} *\\d{1}[A-Z]{1}\\d{1}$)", ErrorMessage = "Zip code is invalid.")]
-        public string Zip { get; set; }
+
         [Display(Name = "State")]
         [Required(ErrorMessage = "State is required")]
         public int StateId { get; set; }
-        [Required(ErrorMessage = " User ID is required")]
-        [DataType(DataType.Date)]
+
+        public virtual State State { get; set; }
+
+        [Required(ErrorMessage = "Zip Code is required")]
+        [Display(Name = "Zip Code")]
+        [StringLength(ContactManagerConstants.MAX_ZIP_CODE_LENGTH, MinimumLength = ContactManagerConstants.MIN_ZIP_CODE_LENGTH)]
+        [RegularExpression("(^\\d{5}(-\\d{4})?$)|(^[ABCEGHJKLMNPRSTVXY]{1}\\d{1}[A-Z]{1} *\\d{1}[A-Z]{1}\\d{1}$)", ErrorMessage = "Zip code is invalid.")] // US or Canada
+        public string Zip { get; set; }
+        /* https://stackoverflow.com/questions/16675176/asp-net-mvc-4-zip-code-validation */
+
+        [Required(ErrorMessage = "The User ID is required in order to map the contact to a user correctly")]
         public string UserId { get; set; }
+
+        [Display(Name = "Full Name")]
         public string FriendlyName => $"{FirstName} {LastName}";
-        [Display(Name = "FullName")]
-        public DateTime Birthday { get; set; }
-        /* public string FriendlyAddress
-           {
-               get
-               {
-                   if (State is null)
-                   {
-                       return "";
-                   }
-                   else
-                   {
-                       if (string.IsNullOrWhiteSpace(StreetAddress1))
-                       {
-                           return $"{City}, {State.Abberviation}, {Zip}";
-                       }
-                       else
-                       {
-                           if (string.IsNullOrWhiteSpace(StreetAddress2))
-                           {
-                               return $"{StreetAddress1}, {City}, {State.Abberviation}, {Zip}";
-                           }
-                           else
-                           {
-                               return $"{StreetAddress1} - {StreetAddress2}, {City}, {State.Abberviation}, {Zip}";
-                           }
-                       }
-                   }
-               }
-           }*/
 
-        [Display(Name = "Address")]
+        [Display(Name = "Full Address")]
         public string FriendlyAddress => State is null ? "" : string.IsNullOrWhiteSpace(StreetAddress1) ? $"{City}, {State.Abbreviation}, {Zip}" :
-                                                                string.IsNullOrWhiteSpace(StreetAddress2)
-                                                                       ? $"{StreetAddress1}, {City},{State.Abbreviation}, {Zip}"
-                                                                       : $"{StreetAddress1} - {StreetAddress2}, {City}, {State.Abbreviation}, {Zip}";
-
+                                                string.IsNullOrWhiteSpace(StreetAddress2)
+                                                    ? $"{StreetAddress1}, {City}, {State.Abbreviation}, {Zip}"
+                                                    : $"{StreetAddress1} - {StreetAddress2}, {City}, {State.Abbreviation}, {Zip}";
 
     }
 }
+                                                    /* public string FriendlyAddress
+                                                        {
+                                                            get
+                                                            {
+                                                                if (State is null)
+                                                                {
+                                                                    return "";
+                                                                }
+                                                                else
+                                                                {
+                                                                    if (string.IsNullOrWhiteSpace(StreetAddress1))
+                                                                    {
+                                                                        return $"{City}, {State.Abberviation}, {Zip}";
+                                                                    }
+                                                                    else
+                                                                    {
+                                                                        if (string.IsNullOrWhiteSpace(StreetAddress2))
+                                                                        {
+                                                                            return $"{StreetAddress1}, {City}, {State.Abberviation}, {Zip}";
+                                                                        }
+                                                                        else
+                                                                        {
+                                                                            return $"{StreetAddress1} - {StreetAddress2}, {City}, {State.Abberviation}, {Zip}";
+                                                                        }
+                                                                    }
+                                                                }
+                                                            }
+                                                        }*/
